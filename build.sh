e4e0113beaa34784ce7f0f1be529b58eabbb0977
JoshuaLeighton1 <joshuaroelf@gmail.com>
2026-08-24 21:52:23 +0200

    Added makemigrations to build script to fix missing tables

--- build.sh
+++ build.sh
@@ -4,7 +4,12 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+
+# 1. Generate the database blueprints
+python manage.py makemigrations
+
+# 2. Build the database tables
 python manage.py migrate
 
-# Add this new line to run your seeder script
+# 3. Add the dummy data
 python setup_data.py
